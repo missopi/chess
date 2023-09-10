b07a09ee9b06c686a96eb7656f1544ab743e0ad0
missopi <missopi@me.com>
2023-09-10 16:48:43 +0100

    Layout pieces class and subclasses plan

---
+++ lib/piece.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+# class for creating chess pieces
+class Piece
+attr_accessor :color, :piece
+
+  def initialize(color, piece)
+    @color = color
+    @piece = piece
+  end
+
+  def valid_moves(current_position)
+  end
+end
+
+# subclass for pawn specific characteristics
+class Pawn < Piece
+  def valid_moves(current_position)
+  end
+
+  # can move 2 spaces on first move
+  def en_passant(current_position)
+  end
+
+  # can upgrade to either queen, rook, bishop or knight when at 8th row
+  def promotion(current_position, piece)
+  end
+end
+
+# subclass for rook specific characteristics
+class Rook < Piece
+  def valid_moves(current_position)
+  end
+end
+
+# subclass for bishop specific characteristics
+class Bishop < Piece
+  def valid_moves(current_position)
+  end
+end
+
+# subclass for knight specific characteristics
+class Knight < Piece
+  def valid_moves(current_position)
+  end
+end
+
+# subclass for queen specific characteristics
+class Queen < Piece
+  def valid_moves(current_position)
+  end
+end
+
+# subclass for king specific characteristics
+class King < Piece
+  def valid_moves(current_position)
+  end
+
+  # once per game king can move 2 spaces and place rook on space skipped over
+  def castling(current_position)
+  end
+end
+
