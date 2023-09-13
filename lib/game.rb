d735a72dd1338bb43482a8692547691cca020f68
missopi <missopi@me.com>
2023-09-13 17:26:37 +0100

    Make method for starting position of pieces
    Will need changing.

--- lib/game.rb
+++ lib/game.rb
@@ -4,8 +4,10 @@ require_relative '../lib/player'
 
 # class for the chess gameplay
 class Game
+  attr_reader :board
+
   def initialize(turn = 1)
-    @board = Board.new
+    @board = Chessboard.new
     @turn = turn
   end
 
@@ -16,6 +18,30 @@ class Game
     puts 'Instructions for how to play chess can be found at'
     puts 'https://en.wikipedia.org/wiki/Chess.'
     puts "\n"
+  end
+
+  def starting_position
+    8.times do |x|
+      board[[1, x]] = Pawn.new(:black)
+      board[[6, x]] = Pawn.new(:white)
+    end
+
+    board[[0, 0]] = Rook.new(:black)
+    board[[0, 1]] = Knight.new(:black)
+    board[[0, 2]] = Bishop.new(:black)
+    board[[0, 3]] = Queen.new(:black)
+    board[[0, 4]] = King.new(:black)
+    board[[0, 5]] = Bishop.new(:black)
+    board[[0, 6]] = Knight.new(:black)
+    board[[0, 7]] = Rook.new(:black)
+    board[[7, 0]] = Rook.new(:white)
+    board[[7, 1]] = Knight.new(:white)
+    board[[7, 2]] = Bishop.new(:white)
+    board[[7, 3]] = Queen.new(:white)
+    board[[7, 4]] = King.new(:white)
+    board[[7, 5]] = Bishop.new(:white)
+    board[[7, 6]] = Knight.new(:white)
+    board[[7, 7]] = Rook.new(:white)
   end
 
   def create_player(number)
@@ -45,3 +71,7 @@ class Game
 
   def game_over?() end
 end
+
+game = Game.new
+game.starting_position
+p game.board
