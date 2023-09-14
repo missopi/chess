7b18cbba19d36cd657b549b61882e3da22a5aa0a
missopi <missopi@me.com>
2023-09-14 12:50:03 +0100

    Start to render visual layout of chessboard

--- lib/render_board.rb
+++ lib/render_board.rb
@@ -3,5 +3,23 @@
 require_relative '../lib/chessboard'
 
 class RenderBoard
-  
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
+  end
+
+  def render
+    8.times do |row|
+      puts ' '
+      8.times do |column|
+        print ' x '
+      end
+    end
+    puts ' '
+  end
+
 end
+
+render = RenderBoard.new(Chessboard.new)
+render.render
