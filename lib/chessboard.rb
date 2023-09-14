8330cfd32d46d6e8eb98168cda02b44d062ea7d3
missopi <missopi@me.com>
2023-09-14 13:09:20 +0100

    Add start_chess method
    Calls self and adds pieces to board

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -6,11 +6,34 @@ require_relative '../lib/piece'
 class Chessboard
   attr_reader :board
 
-  def initialize
-    @board = Array.new(8) { Array.new(8) }
+  def self.start_chess
+    board = self.new
+    8.times do |x|
+      board[[1, x]] = Pawn.new(:black)
+      board[[6, x]] = Pawn.new(:white)
+    end
+
+    board[[0, 0]] = Rook.new(:black)
+    board[[0, 1]] = Knight.new(:black)
+    board[[0, 2]] = Bishop.new(:black)
+    board[[0, 3]] = Queen.new(:black)
+    board[[0, 4]] = King.new(:black)
+    board[[0, 5]] = Bishop.new(:black)
+    board[[0, 6]] = Knight.new(:black)
+    board[[0, 7]] = Rook.new(:black)
+    board[[7, 0]] = Rook.new(:white)
+    board[[7, 1]] = Knight.new(:white)
+    board[[7, 2]] = Bishop.new(:white)
+    board[[7, 3]] = Queen.new(:white)
+    board[[7, 4]] = King.new(:white)
+    board[[7, 5]] = Bishop.new(:white)
+    board[[7, 6]] = Knight.new(:white)
+    board[[7, 7]] = Rook.new(:white)
+    board
   end
 
-  def display_board
+  def initialize
+    @board = Array.new(8) { Array.new(8) }
   end
 
   # setter method to place piece on board
