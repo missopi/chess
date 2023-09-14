31d3664cc5f4694632459ed35763465b366307e0
missopi <missopi@me.com>
2023-09-14 12:48:56 +0100

    Remove pieces and starting position method
    Move to chessboard class

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/chessboard'
 require_relative '../lib/piece'
 require_relative '../lib/player'
+require_relative '../lib/render_board'
 
 # class for the chess gameplay
 class Game
@@ -18,30 +19,6 @@ class Game
     puts 'Instructions for how to play chess can be found at'
     puts 'https://en.wikipedia.org/wiki/Chess.'
     puts "\n"
-  end
-
-  def starting_position
-    8.times do |x|
-      board[[1, x]] = Pawn.new(:black)
-      board[[6, x]] = Pawn.new(:white)
-    end
-
-    board[[0, 0]] = Rook.new(:black)
-    board[[0, 1]] = Knight.new(:black)
-    board[[0, 2]] = Bishop.new(:black)
-    board[[0, 3]] = Queen.new(:black)
-    board[[0, 4]] = King.new(:black)
-    board[[0, 5]] = Bishop.new(:black)
-    board[[0, 6]] = Knight.new(:black)
-    board[[0, 7]] = Rook.new(:black)
-    board[[7, 0]] = Rook.new(:white)
-    board[[7, 1]] = Knight.new(:white)
-    board[[7, 2]] = Bishop.new(:white)
-    board[[7, 3]] = Queen.new(:white)
-    board[[7, 4]] = King.new(:white)
-    board[[7, 5]] = Bishop.new(:white)
-    board[[7, 6]] = Knight.new(:white)
-    board[[7, 7]] = Rook.new(:white)
   end
 
   def create_player(number)
@@ -72,6 +49,3 @@ class Game
   def game_over?() end
 end
 
-game = Game.new
-game.starting_position
-p game.board
