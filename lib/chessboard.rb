5de223dd1a59b4f5db575fed72c2097365222e7e
missopi <missopi@me.com>
2023-09-18 15:08:10 +0100

    Condense start_chess method

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -12,22 +12,16 @@ class Chessboard
       start[[1, c]] = Pawn.new(:black)
       start[[6, c]] = Pawn.new(:white)
     end
-    start[[0, 0]] = Rook.new(:black)
-    start[[0, 1]] = Knight.new(:black)
-    start[[0, 2]] = Bishop.new(:black)
-    start[[0, 3]] = Queen.new(:black)
-    start[[0, 4]] = King.new(:black)
-    start[[0, 5]] = Bishop.new(:black)
-    start[[0, 6]] = Knight.new(:black)
-    start[[0, 7]] = Rook.new(:black)
-    start[[7, 0]] = Rook.new(:white)
-    start[[7, 1]] = Knight.new(:white)
-    start[[7, 2]] = Bishop.new(:white)
-    start[[7, 3]] = Queen.new(:white)
-    start[[7, 4]] = King.new(:white)
-    start[[7, 5]] = Bishop.new(:white)
-    start[[7, 6]] = Knight.new(:white)
-    start[[7, 7]] = Rook.new(:white)
+    [[0, :black], [7, :white]].each do |(row, color)|
+      start[[row, 0]] = Rook.new(color)
+      start[[row, 1]] = Knight.new(color)
+      start[[row, 2]] = Bishop.new(color)
+      start[[row, 3]] = Queen.new(color)
+      start[[row, 4]] = King.new(color)
+      start[[row, 5]] = Bishop.new(color)
+      start[[row, 6]] = Knight.new(color)
+      start[[row, 7]] = Rook.new(color)
+    end
     start
   end
 
