4c716f14bc2d5297642d2120a38114fbda8b6aac
missopi <missopi@me.com>
2023-09-19 17:41:22 +0100

    Test valid moves

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 require_relative '../lib/chessboard'
 require_relative '../lib/piece'
 require_relative '../lib/player'
 require_relative '../lib/render_board'
 
-cb = Chessboard.start_chess
-chess = RenderBoard.new(cb)
-chess.render
+# cb = Chessboard.start_chess
+# chess = RenderBoard.new(cb)
+# chess.render
+
+b = Chessboard.new
+b[[2, 2]] = Queen.new(b, [2, 2], :black)
+p b[[2, 2]].valid_moves
