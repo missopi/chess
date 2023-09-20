f755f0cbfa312fb0faa82784a0b2ccd717b54cf9
missopi <missopi@me.com>
2023-09-20 14:59:07 +0100

    Add move piece method

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -55,4 +55,11 @@ class Chessboard
       row >= 0 &&
       column >= 0
   end
+
+  def move_piece(from, to)
+    piece = self[from]
+    puts 'Not a valid move for this piece' unless piece.valid_move?.include(to) && valid_location?(to)
+    self[from] = nil
+    self[to] = piece
+  end
 end
