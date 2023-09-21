ec9dfe28c12be4c3325a065cd5160de42d800734
missopi <missopi@me.com>
2023-09-21 15:35:36 +0100

    Add input position method
    convert player input of letter and number to coordinates of board

--- lib/player.rb
+++ lib/player.rb
@@ -8,4 +8,12 @@ class Player
     @name = name
     @color = color
   end
+
+  def input_position
+    position = gets.chomp.split(//)
+    position.map do |part|
+      part.upcase!.gsub!(/[A-Z]/) { |m| m.ord - 64 } if ('a'..'g').cover?(part) || ('A'..'G').cover?(part)
+      part.to_i - 1
+    end
+  end
 end
