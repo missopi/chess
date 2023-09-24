0918d6f137cd0a8aea4d1b65b2d01aa78a876606
missopi <missopi@me.com>
2023-09-24 15:22:27 +0100

    Convert player input data to board coordinates
    A to H == rows
    1 to 8 == columns starting from bottom
    
    A1 == bottom left corner

--- lib/player.rb
+++ lib/player.rb
@@ -14,7 +14,25 @@ class Player
     position.map do |part|
       part.upcase!.gsub!(/[A-Z]/) { |m| m.ord - 64 } if ('a'..'g').cover?(part) || ('A'..'G').cover?(part)
     end
-    position[0], position[1] = position[1], position[0]
-    position.map { |part| part.to_i - 1 }
+    numbers = position.map { |num| num.to_i - 1 }
+    numbers[0], numbers[1] = numbers[1], numbers[0]
+    if (numbers[0]).zero?
+      numbers[0] = 7
+    elsif numbers[0] == 1
+      numbers[0] = 6
+    elsif numbers[0] == 2
+      numbers[0] = 5
+    elsif numbers[0] == 3
+      numbers[0] = 4
+    elsif numbers[0] == 4
+      numbers[0] = 3
+    elsif numbers[0] == 5
+      numbers[0] = 2
+    elsif numbers[0] == 6
+      numbers[0] = 1
+    elsif numbers[0] == 7
+      numbers[0] = 0
+    end
+    numbers
   end
 end
