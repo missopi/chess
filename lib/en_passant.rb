9f6fbe9508a9171a136802f7f7662896f382d85e
missopi <missopi@me.com>
2023-10-18 13:35:58 +0100

    Add method to determine piece is on correct row

--- lib/en_passant.rb
+++ lib/en_passant.rb
@@ -2,4 +2,10 @@
 
 # Contains everything for en-passant move
 module EnPassant
+  def correct_row(from)
+    piece = self[from]
+    piece.color == :white ? piece.current_row == 3 : piece.current_row == 4
+  end
+
+  
 end
