9ee2ce952fa90e3de7df1cd0fc25f87c9deefc98
missopi <missopi@me.com>
2023-10-18 13:46:49 +0100

    Add methods to determine what adjacent piece is
    Check if is a pawn and is an opponent

--- lib/en_passant.rb
+++ lib/en_passant.rb
@@ -7,5 +7,21 @@ module EnPassant
     piece.color == :white ? piece.current_row == 3 : piece.current_row == 4
   end
 
-  
+  def left_opponent_pawn?(from)
+    piece = self[from]
+    row, column = from
+    left_opponent = board[row][column - 1]
+    left_opponent.is_a?(Pawn) && left_opponent.color != piece.color
+  end
+
+  def right_opponent_pawn?(from)
+    piece = self[from]
+    row, column = from
+    right_opponent = board[row][column + 1]
+    right_opponent.is_a?(Pawn) && right_opponent.color != piece.color
+  end
+
+  def opponent_adjacent?(from)
+    left_opponent_pawn?(from) || right_opponent_pawn?(from)
+  end
 end
