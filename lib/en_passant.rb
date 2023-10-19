2f6fb9029bfc265bf9e7822db656794e65b1e093
missopi <missopi@me.com>
2023-10-19 13:38:56 +0100

    Add checks in EnPassant if spaces by pawn empty

--- lib/en_passant.rb
+++ lib/en_passant.rb
@@ -20,6 +20,21 @@ module EnPassant
     left_opponent_pawn?(from) || right_opponent_pawn?(from)
   end
 
+  # check if spaces adjacent to pawn are empty before so I know that twostep and adjacent pawn is new move
+  def left_empty?(from)
+    piece = self[from]
+    row, column = from
+    right = board[row][column + 1]
+    right == NoPiece.instance
+  end
+
+  def right_empty?(from)
+    piece = self[from]
+    row, column = from
+    left = board[row][column + 1]
+    left == NoPiece.instance
+  end
+
   def last_move_pawn_twostep?
     last_from = @history[@history.size - 2].first
     last_to = @history[@history.size - 2].last
