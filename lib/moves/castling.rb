752d981a088ff1d2c9ebfea3cc5f19b7b3a7cad9
missopi <missopi@me.com>
2023-10-20 11:32:14 +0100

    Add some pseudocode

--- lib/moves/castling.rb
+++ lib/moves/castling.rb
@@ -2,4 +2,12 @@
 
 # Contains everything for castling move
 module Castling
+  # king identified in chessboard class
+  # identify current player's castles
+  # check that at least one of the castles and the King are still in starting positions
+  # king cannot do if in check
+  # king cannot moves through check
+  # king cannot move into check
+  # king and castle swap places
+  # can only do once per game
 end
