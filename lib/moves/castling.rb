8d80e321dd977c50082787afd5bb927dd6944bd8
missopi <missopi@me.com>
2023-10-20 16:15:53 +0100

    Identify rook used for castling

--- lib/moves/castling.rb
+++ lib/moves/castling.rb
@@ -16,4 +16,28 @@ module Castling
     rook.color == :white && (to == [7, 0] || to == [7, 7]) || 
       rook.color == :black && (to == [0, 0] || to == [0, 7])
   end
+
+
+  # find and identify chosen rook to do castling with
+  def rooks(from, to)
+    king = self[from]
+    rooks = board.flatten.find_all do |piece|
+      piece.is_a?(Rook) && piece.color == king.color
+    end
+    identify_rook(rooks, to)
+  end
+
+  def identify_rook(rooks, to)
+    if to[1] == 6
+      rooks.find { |rook| rook if rook.location[1] == 7 }
+    elsif to[1] == 2
+      rooks.find { |rook| rook if rook.location[1] == 0 }
+    end
+  end
+
+  def kingside_castling()
+  end
+
+  def queenside_castling()
+  end
 end
