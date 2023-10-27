b72d0683c5a9302501e6395bafed6738f9167774
missopi <missopi@me.com>
2023-10-27 16:19:01 +0100

    Add methods to actually make castling move
    Identify king and rook, move king and rook, delete pieces

--- lib/moves/castling.rb
+++ lib/moves/castling.rb
@@ -2,8 +2,17 @@
 
 # Contains everything for castling move
 module Castling
+  # actual castling move
+  def do_castling_move(from, to)
+    king = king_at_start(from)
+    rook = rooks(from, to)
+    move_king(from, to)
+    castling_switch(to, rook)
+    remove_piece(from)
+  end
+
   # check king is in start position
-  def king_at_start?(from)
+  def king_at_start(from)
     king = self[from]
     king.is_a?(King)
     king.color == :white && from == [7, 4] || king.color == :black && from == [0, 4]
@@ -19,16 +28,38 @@ module Castling
   end
 
   def identify_rook(rooks, to)
-    if to[1] == 6
+    row, column = to
+    if column == 6
       rooks.find { |rook| rook if rook.location[1] == 7 }
-    elsif to[1] == 2
+    elsif column == 2
       rooks.find { |rook| rook if rook.location[1] == 0 }
     end
   end
 
-  def kingside_castling(king, rook)
+  def move_king(from, to)
+    king = self[from]
+    self[to] = king
+    king.location = to
   end
 
-  def queenside_castling(king, rook)
+  def kingside_castling(to, rook)
+    row, column = to
+    board[row][column - 1] = rook
+    board[row][column + 1] = NoPiece.instance
+  end
+
+  def queenside_castling(to, rook)
+    row, column = to
+    board[row][column + 1] = rook
+    board[row][column + 2] = NoPiece.instance
+  end
+
+  def castling_switch(to, rook)
+    row, column = to
+    if column == 6
+      kingside_castling(to, rook)
+    elsif column == 2
+      queenside_castling(to, rook)
+    end
   end
 end
