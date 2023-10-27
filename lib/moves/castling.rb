f5afa1cf84a3021a2f97af708c27a4965a5f2c25
missopi <missopi@me.com>
2023-10-27 15:40:09 +0100

    Remove rook_at_start method
    No longer makes sense

--- lib/moves/castling.rb
+++ lib/moves/castling.rb
@@ -9,15 +9,6 @@ module Castling
     king.color == :white && from == [7, 4] || king.color == :black && from == [0, 4]
   end
 
-  # check rook is in start position
-  def rook_at_start?(to)
-    rook = self[to]
-    rook.is_a?(Rook)
-    rook.color == :white && (to == [7, 0] || to == [7, 7]) || 
-      rook.color == :black && (to == [0, 0] || to == [0, 7])
-  end
-
-
   # find and identify chosen rook to do castling with
   def rooks(from, to)
     king = self[from]
@@ -35,9 +26,9 @@ module Castling
     end
   end
 
-  def kingside_castling()
+  def kingside_castling(king, rook)
   end
 
-  def queenside_castling()
+  def queenside_castling(king, rook)
   end
 end
