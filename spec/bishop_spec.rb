82c27648c8ed2d6a2a640daadfe72fc772d9a6c4
missopi <missopi@me.com>
2023-11-11 15:13:30 +0000

    Add black bishop to to_s test

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -4,12 +4,21 @@ require_relative '../lib/pieces'
 
 describe Bishop do
   describe '#to_s' do
-    subject(:bishop_string) { described_class.new(Chessboard.new, [0, 0], :white) }
+    subject(:bishop_white) { described_class.new(Chessboard.new, [0, 0], :white) }
 
     context 'When a white bishop is chosen' do
       it 'displays a white bishop icon on the board' do
-        bishop = bishop_string.to_s
+        bishop = bishop_white.to_s
         expect(bishop).to eq(' ♗ ')
+      end
+    end
+
+    subject(:bishop_black) { described_class.new(Chessboard.new, [0, 0], :black) }
+
+    context 'When a black bishop is chosen' do
+      it 'displays a black bishop icon on the board' do
+        bishop = bishop_black.to_s
+        expect(bishop).to eq(' ♝ ')
       end
     end
   end
