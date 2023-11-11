1173dc8a85622ce7ac775f2782fd94a247310846
missopi <missopi@me.com>
2023-11-11 15:07:40 +0000

    Add to_s tests

--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -2,3 +2,24 @@
 
 require_relative '../lib/pieces'
 
+describe Queen do
+  describe '#to_s' do
+    subject(:queen_white) { described_class.new(Chessboard.new, [0, 0], :white) }
+
+    context 'When a white queen is chosen' do
+      it 'displays a white queen icon on the board' do
+        queen = queen_white.to_s
+        expect(queen).to eq(' ♕ ')
+      end
+    end
+
+    subject(:queen_black) { described_class.new(Chessboard.new, [0, 0], :black) }
+
+    context 'When a black queen is chosen' do
+      it 'displays a black queen icon on the board' do
+        queen = queen_black.to_s
+        expect(queen).to eq(' ♛ ')
+      end
+    end
+  end
+end
