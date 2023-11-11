a1b704e498d9604da2d43915308663a836ac49bf
missopi <missopi@me.com>
2023-11-11 14:56:21 +0000

    Add test for to_s method

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
-require_relative '../lib/pieces/bishop'
+require_relative '../lib/pieces'
+
+describe Bishop do
+  describe '#to_s' do
+    subject(:bishop_string) { described_class.new(Chessboard.new, [0, 0], :white) }
+
+    context 'When a white bishop is chosen' do
+      it 'displays a white bishop icon on the board' do
+        bishop = bishop_string.to_s
+        expect(bishop).to eq(' ♗ ')
+      end
+    end
+  end
+end
