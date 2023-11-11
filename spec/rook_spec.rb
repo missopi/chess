3d6793e05c5ff12a0af606eaedac299c4f399b67
missopi <missopi@me.com>
2023-11-11 15:02:55 +0000

    Add to_s tests

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -2,3 +2,24 @@
 
 require_relative '../lib/pieces'
 
+describe Rook do
+  describe '#to_s' do
+    subject(:rook_white) { described_class.new(Chessboard.new, [0, 0], :white) }
+
+    context 'When a white rook is chosen' do
+      it 'displays a white rook icon on the board' do
+        rook = rook_white.to_s
+        expect(rook).to eq(' ♖ ')
+      end
+    end
+
+    subject(:rook_black) { described_class.new(Chessboard.new, [0, 0], :black) }
+
+    context 'When a black rook is chosen' do
+      it 'displays a black rook icon on the board' do
+        rook = rook_black.to_s
+        expect(rook).to eq(' ♜ ')
+      end
+    end
+  end
+end
