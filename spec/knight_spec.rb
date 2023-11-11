3edcc82922a3f12102c2e2ad43073dc89abd19f2
missopi <missopi@me.com>
2023-11-11 15:05:01 +0000

    Add to_s tests

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -2,3 +2,24 @@
 
 require_relative '../lib/pieces'
 
+describe Knight do
+  describe '#to_s' do
+    subject(:knight_white) { described_class.new(Chessboard.new, [0, 0], :white) }
+
+    context 'When a white knight is chosen' do
+      it 'displays a white knight icon on the board' do
+        knight = knight_white.to_s
+        expect(knight).to eq(' ♘ ')
+      end
+    end
+
+    subject(:knight_black) { described_class.new(Chessboard.new, [0, 0], :black) }
+
+    context 'When a black knight is chosen' do
+      it 'displays a black knight icon on the board' do
+        knight = knight_black.to_s
+        expect(knight).to eq(' ♞ ')
+      end
+    end
+  end
+end
