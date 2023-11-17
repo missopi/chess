00fa0bb5ddd8df44c65b2e4e8165f168175ad3c6
missopi <missopi@me.com>
2023-11-17 11:46:22 +0000

    Add current row test

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,3 +1,14 @@
 # frozen_string_literal: true
 
-require_relative '../lib/pieces/piece'
+require_relative '../lib/pieces'
+
+describe Piece do
+  describe '#current_row' do
+    subject(:piece_row) { described_class.new(Chessboard.new, [0, 0], :white) }
+
+    it "returns the piece's current row" do
+      row = piece_row.current_row
+      expect(row).to eq(0)
+    end
+  end
+end
