fdcf6da8126dd2818a2152e426c35a1ab523e0c5
missopi <missopi@me.com>
2023-11-17 15:34:54 +0000

    Add moves file to link all moves in one place

---
+++ lib/moves.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative '../lib/moves/castling'
+require_relative '../lib/moves/check'
+require_relative '../lib/moves/checkmate'
+require_relative '../lib/moves/en_passant'
+require_relative '../lib/moves/pawn_promotion'
