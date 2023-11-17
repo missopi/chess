d9bb8dd53ac798e31896c091ef351170089af779
missopi <missopi@me.com>
2023-11-17 15:14:01 +0000

    Add test for string method

--- spec/pieces/no_piece_spec.rb
+++ spec/pieces/no_piece_spec.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
-require_relative '../lib/pieces/no_piece'
+require_relative '../../lib/pieces'
+
+describe NoPiece do
+  describe '#to_s' do
+    subject(:no_piece_string) { described_class.instance }
+
+    context 'When there is no piece on the board' do
+      it 'displays a blank space' do
+        no_piece = no_piece_string.to_s
+        expect(no_piece).to be nil
+      end
+    end
+  end
+end
