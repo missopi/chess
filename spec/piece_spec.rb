40dd0989d406dd64d3d6a03ab389b416ef18da34
missopi <missopi@me.com>
2023-11-17 11:47:51 +0000

    Add current column test

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -4,11 +4,20 @@ require_relative '../lib/pieces'
 
 describe Piece do
   describe '#current_row' do
-    subject(:piece_row) { described_class.new(Chessboard.new, [0, 0], :white) }
+    subject(:piece_row) { described_class.new(Chessboard.new, [5, 0], :white) }
 
     it "returns the piece's current row" do
       row = piece_row.current_row
-      expect(row).to eq(0)
+      expect(row).to eq(5)
+    end
+  end
+
+  describe '#current_column' do
+    subject(:piece_column) { described_class.new(Chessboard.new, [0, 4], :white) }
+
+    it "returns the piece's current column" do
+      column = piece_column.current_column
+      expect(column).to eq(4)
     end
   end
 end
