c65965fce228035514c7e2dd3d1063e39bba9e91
missopi <missopi@me.com>
2023-11-20 15:07:42 +0000

    Add start_chess method tests

--- spec/chessboard_spec.rb
+++ spec/chessboard_spec.rb
@@ -3,57 +3,73 @@
 require_relative '../lib/chessboard'
 
 describe Chessboard do
-  describe '#initialize' do
-    subject(:new_board) { described_class.new }
+  subject(:test_board) { described_class.new }
 
+  describe '#start_chess' do
+    subject(:test_start) { described_class.start_chess }
+
+    context 'A new game of chess' do
+      it 'has a Rook in coord [0, 0]' do
+        rook = test_start[[0, 0]]
+        expect(rook).to be_a(Rook)
+      end
+
+      it 'has a King in coord [7, 4]' do
+        king = test_start[[7, 4]]
+        expect(king).to be_a(King)
+      end
+
+      it 'has a black piece in coord [1, 3]' do
+        black_piece = test_start[[1, 3]].color
+        expect(black_piece).to be(:black)
+      end
+    end
+  end
+
+  describe '#initialize' do
     context 'A new board' do
       it 'has 8 rows' do
-        rows = new_board.board.size
+        rows = test_board.board.size
         expect(rows).to eq(8)
       end
 
       it 'has 8 columns' do
-        columns = new_board.board[0].size
+        columns = test_board.board[0].size
         expect(columns).to eq(8)
       end
     end
   end
 
   describe '#[]=' do
-    subject(:place_piece) { described_class.new }
-
     context 'when a valid position is chosen' do
       it 'places a piece' do
         loc = [0, 3]
-        place_piece[loc] = 'KING'
-        place = place_piece.board[0][3]
-        expect(place).to eq('KING')
+        test_board[loc] = 'KING'
+        place_piece = test_board.board[0][3]
+        expect(place_piece).to eq('KING')
       end
     end
   end
 
   describe '#[]' do
-    subject(:get_piece) { described_class.new }
     context 'when a valid position is chosen' do
       before do
         loc = [3, 4]
-        get_piece[loc] = 'QUEEN'
+        test_board[loc] = 'QUEEN'
       end
 
       it 'returns piece' do
-        get = get_piece[[3, 4]]
+        get = test_board[[3, 4]]
         expect(get).to eq('QUEEN')
       end
     end
   end
 
   describe '#valid_location' do
-    subject(:valid_board) { described_class.new }
-
     context 'A location out of bounds' do
       it 'returns false' do
         out = [-1, 10]
-        location = valid_board.valid_location?(out)
+        location = test_board.valid_location?(out)
         expect(location).to be false
       end
     end
@@ -61,7 +77,7 @@ describe Chessboard do
     context 'A valid location' do
       it 'returns true' do
         valid = [5, 5]
-        location = valid_board.valid_location?(valid)
+        location = test_board.valid_location?(valid)
         expect(location).to be true
       end
     end
