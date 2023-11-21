09fa07a87e314b3470cda4796a0d1c4da6156303
missopi <missopi@me.com>
2023-11-21 15:06:54 +0000

    Add tests for create_player method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,32 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game'
+
+describe Game do
+  subject(:test_game) { described_class.new(test_board, RenderBoard) }
+  subject(:test_board) { Chessboard.start_chess }
+
+  describe '#create_player' do
+    context 'at the start of play' do
+      before do
+        allow(test_game).to receive(:puts)
+        allow(test_game).to receive(:gets).and_return('Sophie')
+      end
+
+      it 'creates a new player' do
+        test_player = test_game.create_player(1)
+        expect(test_player.name).to eq('Sophie')
+      end
+
+      it 'assigns a player a colour' do
+        test_player = test_game.create_player(1)
+        expect(test_player.color).to eq(:white)
+      end
+    end
+  end
+
+  describe '#assign_current_player' do
+  end
+
+
+end
