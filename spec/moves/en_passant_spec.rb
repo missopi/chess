a2b1f0ea7f26fcc3cff962f871f3bac70f73fe0d
missopi <missopi@me.com>
2023-11-22 13:26:28 +0000

    Add test for left opponent pawn

--- spec/moves/en_passant_spec.rb
+++ spec/moves/en_passant_spec.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
 
 require_relative '../../lib/moves'
+
+describe EnPassant do
+  subject(:test_board) { Chessboard.new }
+  subject(:test_pawn) { Pawn.new(test_board, [4, 4], :white) }
+
+  describe '#left_opponent_pawn?' do
+    context 'when an opponent pawn it to the left' do
+      before do
+        allow(test_board[[4, 3]] = Pawn.new(test_board, [4, 3], :black))
+      end
+
+      it 'returns true' do
+        result = test_board.left_opponent_pawn?([4, 4])
+        expect(result).to be true
+      end
+    end
+  end
+end
