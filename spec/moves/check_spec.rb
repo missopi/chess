ce0a67dfe976241d0a3c2ce69f0056f9d878c163
missopi <missopi@me.com>
2023-11-22 15:02:05 +0000

    Add tests for check method

--- spec/moves/check_spec.rb
+++ spec/moves/check_spec.rb
@@ -2,3 +2,35 @@
 
 require_relative '../../lib/moves'
 require_relative '../../lib/chessboard'
+require_relative '../../lib/pieces'
+
+describe Check do
+  subject(:test_board) { Chessboard.new }
+
+  describe '#check?' do
+    before do
+      allow(test_board[[0, 0]] = King.new(test_board, [0, 0], :white))
+      allow(test_board[[0, 4]] = King.new(test_board, [0, 4], :black))
+    end
+
+    context 'if the king is in check' do
+      before do
+        allow(test_board[[0, 2]] = Rook.new(test_board, [0, 2], :black))
+        allow(test_board[[2, 2]] = Queen.new(test_board, [2, 2], :black))
+        allow(test_board[[2, 0]] = Rook.new(test_board, [2, 0], :black))
+      end
+
+      it 'returns true' do
+        check = test_board.check?(:white)
+        expect(check).to be true
+      end
+    end
+
+    context 'if the king is not in check' do
+      it 'returns false' do
+        check = test_board.check?(:black)
+        expect(check).to be false
+      end
+    end
+  end
+end
