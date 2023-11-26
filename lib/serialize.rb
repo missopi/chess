fdce30d8633af8295dae7bfda0072cd4931440f4
missopi <missopi@me.com>
2023-11-26 13:31:36 +0000

    Create serialize file to save and load the game
    Make class and loayout of some methods needed

---
+++ lib/serialize.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# class that handles everything to do with saving and loading the game
+class Serialize
+  def create_filename
+  end
+  
+  def save_game 
+  end
+
+  def load_game
+  end
+
+  def saved_games
+  end
+
+  def display_saved_games
+  end
+
+  def choose_game_to_load
+  end
+
+  def resume_game
+  end
+end
