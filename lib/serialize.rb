2f9408fe57b9e69a3bc1614ee82675d35806305c
missopi <missopi@me.com>
2023-11-26 13:55:02 +0000

    Complete save_game method

--- lib/serialize.rb
+++ lib/serialize.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 # class that handles everything to do with saving and loading the game
 class Serialize
   def create_filename
   end
 
-  def save_game
+  def save_game(current_game)
     Dir.mkdir('saved_games') unless Dir.exist?('saved_games')
 
-    
+    filename = create_filename
+    game = YAML.dump(current_game)
+    File.open(File.join(Dir.pwd, "/saved_games/#{filename}.yaml"), 'w') { |file| file.write game }
+    puts "Game saved in saved_games/#{filename}"
   end
 
   def load_game
