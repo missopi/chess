9d828877ce413543f19551edba0a3475acf4e779
missopi <missopi@me.com>
2023-12-01 12:08:07 +0000

    Add tests for save_game method

--- spec/serialize_spec.rb
+++ spec/serialize_spec.rb
@@ -20,6 +20,25 @@ describe Serialize do
     end
   end
 
+  describe '#save_game' do
+    before do
+      allow(Dir).to receive(:mkdir)
+      allow(test_game).to receive(:puts)
+      allow(test_game).to receive(:gets).and_return('test')
+      allow(File).to receive(:open)
+    end
+
+    it 'sends message to create a directory' do
+      expect(Dir).to receive(:mkdir).once
+      test_game.save_game(test_game)
+    end
+
+    it 'sends message to create a file' do
+      expect(File).to receive(:open).once
+      test_game.save_game(test_game)
+    end
+  end
+
   describe '#saved_games' do
     xit 'returns saved games' do
     end
